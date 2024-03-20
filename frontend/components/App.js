33e3bf55b633f7d8c678744daf814780778dce03
brightgirlkell <teshara2210@icloud.com>
2024-03-20 11:20:26 -0400

    1 updated files

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import { Routes, Route, NavLink} from 'react-router-dom';
+
 
 function App() {
   return (
     <div id="app">
       <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/order">Order</NavLink>
         {/* NavLinks here */}
       </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/order" element={<Form />}/>
+      </Routes>
       {/* Route and Routes here */}
       <Home />
       <Form />
